bc0bb201113e07a570dad389d3519f3bed7a78d9
Denis Kutsenko <deniskutchenko008@gmail.com>
2020-02-03 16:54:10 +0100

    Add program

---
+++ WpfApp1/WpfApp1/Servises/FileIOServises.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WpfApp1.Models;
+
+namespace WpfApp1.Servises
+{
+    class FileIOServises
+    {
+        public FileIOServises(string path)
+        {
+            PATH = path;
+        }
+        private readonly string PATH;
+        public BindingList<ToDoModels> LoadData()
+        {
+            var fileExsist = File.Exists(PATH);
+            if (!fileExsist)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<ToDoModels>();
+            }
+            using(var readrer = File.OpenText(PATH))
+            {
+                var fileText = readrer.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToDoModels>>(fileText);
+            }
+            
+        }
+
+        public void SavedData(object toDoDataList) {
+            
+            using(StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(toDoDataList);
+                writer.Write(output);
+            }
+
+        }
+    }
+}
